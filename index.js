38576cb6f605a910e35018658be2274e18553e3f
Gustavo Soares Rodrigues <825231371@ulife.com.br>
2025-09-15 21:27:15 -0300

    aula 1

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+const lembretes = {}
+app.use(bodyParser.json)
+let contador = 0
+
+// Listar lembretes
+app.get("/lembretes", (req, res) => {
+    res.send(lembretes)
+})
+
+// Criar lembretes
+app.put("/lembretes", (req, res) => {
+    contador++
+    const {texto} = req.body
+    lembretes[contador] = {contador, texto}
+    res.status(201).send(lembretes[contador])
+})
+
+app.listen(4050, () => {
+    console.log("Lembretes. Porta 4000")
+})
